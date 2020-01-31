0a9874b9a0a889bcef88a364a3be0a7189d2b8e1
michealtk <55165489+michealtk@users.noreply.github.com>
2020-01-31 16:19:26 -0600

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCmYQTcfIQ8IyRcxkz/+UinxjK89WQoIzANqSjE+ju+fWnqb5atF8IJunO7Fr1TfKRObB+kjsK5xLvH6QKDriffT48a9CpDzotLhftitq3Z6yJffpq7+ZW6Im9uyYBN+XLl5qtCvFnnblUOA9wMwtwBRIYwrtl5/pnRyg0zv82ClilFPBvY0QbzfKk3RYRsn/9Z49TF0cyn/olvm2H3uhSBS2vXFHq3JS/OGIUv0DEWIyauRyc4EDjhfsYxvy7kk9n0sHBg4aIqiVkcqw6TBxQILt/nwlNnNM4xN94SJgPFy/uP3B2g7ZLGYTaPsLKoIikYfdQUCrYyFl2XNu/+YwKZ',
 	}  
 }
